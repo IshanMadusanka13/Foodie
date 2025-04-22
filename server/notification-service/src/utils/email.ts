4940224d990b0e0bc6a46c3dcdf835a9dfa66691
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-04-22 23:58:27 +0530

    Changed email.ts file

--- server/notification-service/src/utils/email.ts
+++ server/notification-service/src/utils/email.ts
@@ -4,15 +4,15 @@ export const sendEmail = async (to: string, subject: string, message: string) =>
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: process.env.EMAIL_USER!,
+            pass: process.env.EMAIL_PASS!,
         }
     });
 
     await transporter.sendMail({
         from: process.env.EMAIL_USER,
         to,
-        subject,
+        subject: 'Order Confirmation',
         text: message
     });
 };
