d11ebbf887140f24c9595c94145cbec581c0fbd8
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-04-23 00:21:07 +0530

    Changed app.ts file

--- server/notification-service/src/app.ts
+++ server/notification-service/src/app.ts
@@ -1,27 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import http from 'http';
 import { Server } from 'socket.io';
-import dotenv from 'dotenv';
-import notificationRoutes from './routes/notificationRoutes';
+import { Server as HttpServer } from 'http';
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: "*"}
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/notifications', notificationRoutes);
-
-io.on('connection', (socket) => {
-    console.log('Client connected:', socket.id);
-
-    socket.on('newOrder', (data) => {
-        socket.broadcast.emit('orderUpdate', data);
+export const initializeSocket = (server: HttpServer) => {
+    const io = new Server(server, {
+        cors: { origin: '*' }
     });
-});
 
-export default app;
+    io.on('connection', (socket) => {
+        console.log('New client connected');
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnected');
+        });
+    });
+
+    return io;
+};
