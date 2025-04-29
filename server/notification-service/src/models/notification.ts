37446be97ce81a4647aa395df7e1bfb6f70b6b11
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-04-29 10:17:04 +0530

    Add all files

--- server/notification-service/src/models/notification.ts
+++ server/notification-service/src/models/notification.ts
@@ -1,10 +1,24 @@
 import mongoose from 'mongoose';
 
 const notificationSchema = new mongoose.Schema({
-    userEmail: { type: String, required: true },
-    userPhone: { type: String, required: true },
+    userEmail: { 
+        type: String, 
+        required: function(this: any) {
+            return this.type === 'email' || this.type === 'both';
+        }
+    },
+    userPhone: { 
+        type: String, 
+        required: function(this: any) {
+            return this.type === 'sms' || this.type === 'both';
+        }
+    },
     message: { type: String, required: true},
-    type: { type: String, enum: ['email' , 'sms' , 'both'], default: 'both' },
+    type: { 
+        type: String, 
+        enum: ['email' , 'sms' , 'both'], 
+        default: 'both' 
+    },
     sentAt: { type: Date, default: Date.now }
 });
 
